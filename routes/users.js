0a7c4d309dfff67480371171cfbcb9957be24a4f
Aakash <akashchaudhary12121@gmail.com>
2021-10-09 16:37:00 +0530

    Edit user with id added

--- routes/users.js
+++ routes/users.js
@@ -45,4 +45,23 @@ router.delete('/:id', (req, res) => {
     res.send(`user with the id: ${id} deleted to the database`);
 });
 
+router.patch('/:id', (req,res) => {
+    const { id } = req.params;
+    const { firstName, lastName , age } = req.body;
+    const user = users.find((user) => user.id === id);
+
+    if(firstName){
+        user.firstName = firstName;
+    }
+    if(lastName){
+        user.lastName = lastName;
+    }
+    if(age){
+        user.age = age;
+    }
+    
+    res.send(`user with the id: ${id} has been updated`);
+
+})
+
 export default router;
