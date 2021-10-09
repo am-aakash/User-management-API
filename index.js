0ef42ac3a10e505359f2522363e4689adb879dac
Aakash <akashchaudhary12121@gmail.com>
2021-10-09 16:00:32 +0530

    user Data from postman

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
 
 app.get('/', (req,res) => {
     console.log('[TEST]!');
     res.send('Hello from Homepage');
 });
 
-app.listen(PORT, ()=> console.log(`Server running on port: http://localhost${PORT}`));
+app.listen(PORT, ()=> console.log(`Server running on port: http://localhost:${PORT}`)); 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-    res.send('Hello');
+const users = [
+    // {
+    //     firstName: "John",
+    //     lastName: "Doe",
+    //     age: 25
+    // },
+    // {
+    //     firstName: "Jane",
+    //     lastName: "Doe",
+    //     age: 21
+    // },
+]
+
+router.get('/', (req, res) => {
+    // console.log(users);
+    res.send(users);
 } );
+
+router.post('/', (req,res)=> {
+    // console.log('POST ROUTE REACHED');
+    // console.log(req.body);
+    const user= req.body;
+    users.push({ ...user, id:  uuidv4()});
+
+    res.send(`user with the name: ${user.firstName} added to the database`);
+})
 
 export default router;
